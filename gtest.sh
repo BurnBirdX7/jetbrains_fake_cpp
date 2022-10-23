492dddf4aca5c56144d70199fe8a640755ef3106
BurnBird <artemiy.lazarevx7@gmail.com>
2022-10-23 04:19:43 +0300

    Changed working directory of tests in gtest.sh

--- gtest.sh
+++ gtest.sh
@@ -1,5 +1,5 @@
 
 [ ! -d "build" ] && echo "You need to build with ./build.sh first..." && exit 1
 
-cmake --build build --target test &&
-  ./build/gtest/test || echo "Failed to build and run tests"
+cmake --build build --target test && cd build/gtest &&
+  ./test || echo "Failed to build and run tests"
