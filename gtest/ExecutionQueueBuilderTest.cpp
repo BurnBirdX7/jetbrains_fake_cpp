e5ccadd86d80a3860776889cc43f766b62f8131b
Artemiy Lazarev <artemiy.lazarevx7@gmail.com>
2022-10-29 03:05:10 +0300

    Updated unit-tests for ExecutionQueueBuilder...

--- gtest/ExecutionQueueBuilderTest.cpp
+++ gtest/ExecutionQueueBuilderTest.cpp
@@ -1,5 +1,6 @@
-
 #include <filesystem>
+#include <fstream>
+#include <chrono>
 
 #include <gtest/gtest.h>
 #include <yaml-cpp/yaml.h>
@@ -83,3 +84,102 @@ TEST(ExecutionQueueBuilderTest, BuildDifferentBranches) {
     EXPECT_EQ(queue.size(), n + 4) << queue.size() << '\n' << builder;
 }
 
+
+void emitTask(YAML::Emitter& emitter,
+              std::string const& name,
+              std::vector<std::string> const& dependencies)
+{
+    emitter << YAML::Key << name;
+    emitter << YAML::Value << YAML::BeginMap;
+    emitter << YAML::Key << "run" << YAML::Value << "ls";
+    emitter << YAML::Key << "target" << YAML::Value << name;
+    emitter << YAML::Key << "dependencies" << YAML::Value << YAML::BeginSeq;
+    for (auto const& dep : dependencies) {
+        emitter << dep;
+    }
+    emitter << YAML::EndSeq << YAML::EndMap;
+}
+
+std::pair<size_t, std::string> generateLongDoc(size_t n, std::string const& filename) {
+    YAML::Emitter emitter;
+
+    emitter << YAML::BeginMap;
+    std::string prev_task = "task_0";
+    emitTask(emitter, prev_task, {"test2.yaml"});
+    for (size_t i = 1; i <= n; ++i) { // Generate 50 * 3
+        std::string task = "task_" + std::to_string(i);
+        std::string left_task = task + "_left";
+        std::string right_task = task + "_right";
+
+        emitTask(emitter, right_task, {prev_task});
+        emitTask(emitter, left_task, {prev_task});
+        emitTask(emitter, task, {left_task, right_task});
+
+        prev_task = task;
+
+        /*
+         * Builds dependencies:
+         *                 /---left_task<----\
+         * last_task <---<|                  |<---task <---
+         *                \---right_task<---/
+         *
+         */
+    }
+    emitter << YAML::EndMap;
+
+    std::ofstream file(filename);
+    file << emitter.c_str();
+
+    return {n * 3 + 1, prev_task};
+}
+
+TEST(ExecutionQueueBuilderTest, ExecuteOkTask) {
+    auto builder = ExecutionQueueBuilder(doc);
+    builder.build("exec");
+
+    size_t count = 0;
+
+    auto executor = [&count](Task::ptr const& task) {
+        ++count;
+        return std::system(task->cmd().c_str());
+    };
+
+    auto ret = builder.execute(executor);
+    EXPECT_EQ(ret, 0);
+    EXPECT_EQ(count, 3);
+}
+
+TEST(ExecutionQueueBuilderTest, ExecuteFailingTask) {
+    auto builder = ExecutionQueueBuilder(doc);
+    builder.build("failing_exec");
+
+    size_t count = 0;
+
+    auto executor = [&count](Task::ptr const& task) {
+        ++count;
+        return std::system(task->cmd().c_str());
+    };
+
+    auto ret = builder.execute(executor);
+    EXPECT_NE(ret, 0);
+    EXPECT_EQ(count, 3);
+}
+
+TEST(ExecutionQueueBuilderTest, BuildFromSuperlongFile) {
+    const std::string FILENAME = "long_conf.yaml";
+    const size_t TASKS = 50;
+    auto [n, task] = generateLongDoc(50, FILENAME);
+    auto long_doc = YAML::LoadFile(FILENAME);
+
+    auto builder = ExecutionQueueBuilder(long_doc);
+
+    auto begin = std::chrono::steady_clock::now();
+    builder.build("task_" + std::to_string(TASKS / 2));
+    builder.build(task);
+    auto end = std::chrono::steady_clock::now();
+    auto dur = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+    std::cout << "\t\t\tExecution queue build time: " << dur.count() << " ms\n";
+
+    ASSERT_FALSE(builder.failed());
+    ASSERT_EQ(builder.getExecutionQueue().size(), n);
+}
